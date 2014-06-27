760c1dcafe917eea389cc0375b024857d0f7a3b0
Bryan Bishop <kanzure@gmail.com>
2014-06-27 18:35:12 -0500

    include basic test explanations

--- tests/model/test_basic.py
+++ tests/model/test_basic.py
@@ -5,10 +5,16 @@ import modelo.trait.trait_types as field
 
 class BasicModelTests(unittest.TestCase):
     def test_instantiation(self):
+        """
+        Model.__init__ should work (no params).
+        """
         model = Model()
         self.assertIsInstance(model, Model)
 
     def test_to_dict(self):
+        """
+        Model.to_dict should create a dictionary.
+        """
         model = Model()
         result = model.to_dict()
 
@@ -19,9 +25,15 @@ class BasicModelTests(unittest.TestCase):
         self.assertEqual(result, {})
 
     def test_create_no_params(self):
+        """
+        Model.create should work when given no params.
+        """
         model = Model.create()
         self.assertTrue(isinstance(model, Model))
 
-    def test_create_empty_dict(self):
+    def test_create_empty_dict_param(self):
+        """
+        Model.create should work when given an empty dictionary.
+        """
         model = Model.create({})
         self.assertTrue(isinstance(model, Model))
