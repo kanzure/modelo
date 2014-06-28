8897c06cc782d0e4592d23f65be0f4f83fd2e29a
Bryan Bishop <kanzure@gmail.com>
2014-06-28 08:12:30 -0500

    test Model.update with an integer

--- tests/model/test_update.py
+++ tests/model/test_update.py
@@ -6,13 +6,30 @@ import modelo.trait.trait_types as field
 class ModelUpdateTests(unittest.TestCase):
     def setUp(self):
         class Car(Model):
-            number = field.Integer(1)
-            color = field.String("blue")
+            number = field.Integer()
+            color = field.String()
 
-        self.car = Car.create()
+        self.car = Car.create({
+            "number": 1,
+            "color": "blue",
+        })
 
     def test_update_empty(self):
         """
         Model.update should work with empty data.
         """
         self.car.update({})
+
+    def test_update_integer(self):
+        """
+        Model.update should update some integer trait.
+        """
+        original_value = self.car.number
+        new_value = original_value + 1
+
+        # perform the actual update
+        self.car.update({
+            "number": new_value,
+        })
+
+        self.assertEqual(self.car.number, new_value)
