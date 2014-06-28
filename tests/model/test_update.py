395de0a8d48a50367415afda5b23378f1b6ddb38
Bryan Bishop <kanzure@gmail.com>
2014-06-28 08:03:39 -0500

    use specific model when testing update

--- tests/model/test_update.py
+++ tests/model/test_update.py
@@ -4,9 +4,15 @@ from modelo.model.model import Model
 import modelo.trait.trait_types as field
 
 class ModelUpdateTests(unittest.TestCase):
+    def setUp(self):
+        class Car(Model):
+            number = field.Integer(1)
+            color = field.String("blue")
+
+        self.car = Car.create()
+
     def test_update_empty(self):
         """
         Model.update should work with empty data.
         """
-        model = Model()
-        model.update({})
+        self.car.update({})
