a2e07129cd35f1eda907e02b54b3cb5f52608de1
Bryan Bishop <kanzure@gmail.com>
2014-09-06 14:02:31 -0500

    version bump: v0.0.3

--- modelo/__init__.py
+++ modelo/__init__.py
@@ -5,8 +5,8 @@ modelo model library
 """
 
 __title__ = "modelo"
-__version__ = "0.0.2"
-__build__ = 0x000002
+__version__ = "0.0.3"
+__build__ = 0x000003
 __license__ = "BSD"
 
 from .model.model import Model
